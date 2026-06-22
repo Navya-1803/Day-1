827d93c6a3150f6d83e4237fd82d1a1e613e15ed
Navya <navya3989@gmail.com>
2026-06-22 14:50:28 +0530

    Question 3 solution

---
+++ Q3.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class Q3 {
+    static String s = "Java";
+    static String b = "Java";
+
+    public static void main(String[] args) {
+        if(s.equals(b)){
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+
+}
