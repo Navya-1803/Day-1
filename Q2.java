193e944b9bdadff92720a190660bce38936bf910
Navya <navya3989@gmail.com>
2026-06-22 14:48:27 +0530

    Question 2 soultion

---
+++ Q2.java
@@ -0,0 +1,25 @@
+package Day1;
+
+public class Q2 {
+    static byte a;
+    static short s;
+    static char c;
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    static boolean b;
+
+    public static void main(String[] args) {
+
+        System.out.println("Default value of byte:" + a);
+        System.out.println("Default value of short:" + s);
+        System.out.println("Default value of char:" + c);
+        System.out.println("Default value of int:" + i);
+        System.out.println("Default value of long:" + l);
+        System.out.println("Default value of float:" + f);
+        System.out.println("Default value of double:" + d);
+        System.out.println("Default value of boolean:" + b);
+
+    }
+}
